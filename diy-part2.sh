317e3573910a682b4b171171812f5b5a037d3d57
levinhuang2013 <152769562+levinhuang2013@users.noreply.github.com>
2024-10-15 22:30:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,7 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 #git clone -b js https://github.com/gngpp/luci-theme-design package/luci-theme-design
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
-#git clone https://github.com/sirpdboy/netspeedtest package/homebox
+git clone https://github.com/sirpdboy/netspeedtest package/homebox
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 
 # Modify default WiFi SSID
